e435affe8d7dd27f048e2ffba9daa848317beda3
justinandrewfien <jfien12@live.com>
2017-02-15 22:11:29 -0700

    Home and Document changes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,27 @@
 <template>     
+<div>
+<h3>What is Hubot?</h3>
 
+<p>Hubot is your company's robot. Install him in your company to dramatically improve and reduce employee efficiency.</p>
+
+<h3>No seriously, what is Hubot?</h3>
+
+<p>GitHub, Inc., wrote the first version of Hubot to automate our company chat room. Hubot knew how to deploy the site, automate a lot of tasks, and be a source of fun in the company. Eventually he grew to become a formidable force in GitHub. But he led a private, messy life. So we rewrote him.</p>
+
+<p>Today's version of Hubot is open source, written in CoffeeScript on Node.js, and easily deployed on platforms like Heroku. More importantly, Hubot is a standardized way to share scripts between everyone's robots.</p>
+
+<h3>What can Hubot do?</h3>
+
+<p>We ship Hubot with a small group of core scripts: things like posting images, translating languages, and integrating with Google Maps.We also maintain a repository of community Hubot scripts and an organization of community Hubot packages that you can add to your own robot.</p>
+
+<p>The real fun happens when you add your own scripts. Be sure to personalize your Hubot, too; your company's robot should be a place full of inside jokes, custom integrations, and general merriment.</p>
+
+<h3>How do I write my own Hubot scripts?</h3>
+
+<p>Check out this documentation for writing your own Hubot scripts. Then the sky's the limit; just add them to your generated `scripts` directory.</p>
+
+<p>If you write a Hubot script for taking over the world, please let us know.</p>
+</div>
 
 </template>
 
